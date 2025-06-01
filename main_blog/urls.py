fa2ddeada0338f769ae0b691c61e43876190090a
Pranav Pratheek Malleboyina <pranav.msadmissions@gmail.com>
2025-06-01 03:22:33 -0500

    set the views and URLs for the home and about section

--- main_blog/urls.py
+++ main_blog/urls.py
@@ -3,5 +3,6 @@ from main_blog import admin
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='main-blog-home'),
+    path('about/', views.about, name='main-blog-about'),
 ]
--- main_blog/views.py
+++ main_blog/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        "author":"Corey Anderson",
+        "title":"My greatest memory",
+        "content":"How I played an important part in MI qualifying in 2014",
+        "date_posted":"27th May, 2014"
+    },
+    {
+        "author":"Kane Willaimson",
+        "title":"My worst memory",
+        "content":"How a cricket rule robbed us of a world cup in 2019",
+        "date_posted":"27th June, 2019"
+    }
+    
+]
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Pranav's Personal Blog</h1>")
+    context= {
+        "posts":posts
+    }
+    return render(request,'main_blog/home.html',context)
+
+def about(request):
+    return render(request,'main_blog/about.html',{'title':'About section'})
