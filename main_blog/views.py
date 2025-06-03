f0601a478d23e6b57477e3cf4c0d81c39fd60463
Pranav Pratheek Malleboyina <pranav.msadmissions@gmail.com>
2025-06-03 00:09:52 -0500

    Made changes to main_blog application for user registration

--- main_blog/views.py
+++ main_blog/views.py
@@ -2,21 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
-posts = [
-    {
-        "author":"Corey Anderson",
-        "title":"My greatest memory",
-        "content":"How I played an important part in MI qualifying in 2014",
-        "date_posted":"27th May, 2014"
-    },
-    {
-        "author":"Kane Willaimson",
-        "title":"My worst memory",
-        "content":"How a cricket rule robbed us of a world cup in 2019",
-        "date_posted":"27th June, 2019"
-    }
-    
-]
 # Create your views here.
 def home(request):
     context= {
