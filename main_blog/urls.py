b7628ce04663bb671b02580767c4ee145b960b18
Pranav Pratheek Malleboyina <pranav.msadmissions@gmail.com>
2025-06-09 12:41:04 -0500

    Modified the logic for the initial page load(rendering the login) and making sure user is logged in before accessing info

--- main_blog/urls.py
+++ main_blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
 from main_blog import admin
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='main-blog-home'),
+    path('', auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('home/', views.home, name='main-blog-home'),
     path('about/', views.about, name='main-blog-about'),
 ]
