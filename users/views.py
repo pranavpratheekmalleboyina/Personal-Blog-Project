2d58e034530d93cdac614abadc41cb0c617f2054
Pranav Pratheek Malleboyina <pranav.msadmissions@gmail.com>
2025-06-09 12:20:58 -0500

    Added logic for adding and updating profile information for a user

--- users/views.py
+++ users/views.py
@@ -1,23 +1,43 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
 
 # Create your views here.
 def registerUser(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             form.save()
             username= form.cleaned_data.get('username')
             messages.success(request,f'Account created successfully! Please login ')
             return redirect('login')
+        else:
+            messages.error(request, 'There was a problem with your form. Please check the fields.')
     else:
         form = UserRegisterForm()
     return render(request,"users/register.html",{'form': form})
 
 @login_required
 def userProfile(request):
-    return render(request,"users/profile.html")
+    if request.method == 'POST':
+        userForm = UserUpdateForm(request.POST,instance=request.user)
+        profileForm = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+        
+        if userForm.is_valid() and profileForm.is_valid():
+            userForm.save()
+            profileForm.save()
+            messages.success(request,f'Your profile information has been updated!!')
+            return redirect('profile')
+    else:
+        userForm = UserUpdateForm(instance=request.user)
+        profileForm = ProfileUpdateForm(instance=request.user.profile)
+                
+    context={
+        'userForm':userForm,
+        'profileForm':profileForm
+    }
+    
+    return render(request,"users/profile.html",context)
     
 
