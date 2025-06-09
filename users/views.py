afecad07e8fa337799e68c06bec7150156362ce5
Pranav Pratheek Malleboyina <pranav.msadmissions@gmail.com>
2025-06-09 05:00:40 -0500

    Added code for rendering the user profile and modified the register logic to redirect to login after registration

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 # Create your views here.
@@ -9,10 +10,14 @@ def registerUser(request):
         if form.is_valid:
             form.save()
             username= form.cleaned_data.get('username')
-            messages.success(request,f'Account created successfully for { username }!')
-            return redirect('main-blog-home')
+            messages.success(request,f'Account created successfully! Please login ')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request,"users/register.html",{'form': form})
+
+@login_required
+def userProfile(request):
+    return render(request,"users/profile.html")
     
 
