36b41f0d6d56eafc78258b375140caae46a70bfc
Pranav Pratheek Malleboyina <pranav.msadmissions@gmail.com>
2025-06-11 13:18:56 -0500

    Created the URLs and the views for the CUD operations

--- main_blog/urls.py
+++ main_blog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
 from main_blog import admin
+from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView
 from . import views
 
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
-    path('home/', views.home, name='main-blog-home'),
+    path('post/<int:pk>', PostDetailView.as_view(),name='post-detail'),
+    path('post/new', PostCreateView.as_view(),name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(),name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(),name='post-delete'),
+    path('home/', PostListView.as_view(), name='main-blog-home'),
     path('about/', views.about, name='main-blog-about'),
 ]
--- main_blog/views.py
+++ main_blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.urls import reverse_lazy
 
 # We create our views here
 @login_required  #this is for the home page i.e. the one we return to where all the info is displayed
@@ -11,6 +14,47 @@ def home(request):
     } 
     return render(request,'main_blog/home.html',context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'main_blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+    
+    def form_valid(self,form):
+        form.instance.author = self.request.user    
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+    
+    def form_valid(self,form):
+        form.instance.author = self.request.user    
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+        
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('main-blog-home')
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
 #This is the about section where a user can describe about themselves
 @login_required
 def about(request):
