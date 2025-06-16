15f8b5ffb3336df1bd2d73aca5b7ad025674f241
Pranav Pratheek Malleboyina <pranav.msadmissions@gmail.com>
2025-06-16 17:58:50 -0500

    Added the deployed allowed hosts and the secret key

--- django_personal_blog/settings.py
+++ django_personal_blog/settings.py
@@ -17,17 +17,16 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mtat5e$x9!l#-x*bxhs)fru$6(+q%twf2=#tlvyjq(x$r2qjr7'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pranavpersonalblog-eaca44afb934.herokuapp.com']
 
 
 # Application definition
