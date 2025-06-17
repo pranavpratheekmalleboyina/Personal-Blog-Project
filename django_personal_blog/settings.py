ae4f9868f6e697158e84808b0f73cbf0d73cd0a0
Pranav Pratheek Malleboyina <pranav.msadmissions@gmail.com>
2025-06-16 19:00:10 -0500

    Added the django-heroku dependency

--- django_personal_blog/settings.py
+++ django_personal_blog/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,4 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = config("EMAIL_USER")
 EMAIL_HOST_USER = config("EMAIL_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
+django_heroku.settings(locals())
